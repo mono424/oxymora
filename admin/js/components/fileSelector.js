f0a262b822a194aca094c609ebf61811d6bf84d8
Khadim Fall <khadimfall0602@gmail.com>
2016-11-30 00:36:55 +0100

    worked on file selector design

--- admin/js/components/fileSelector.js
+++ admin/js/components/fileSelector.js
@@ -21,8 +21,45 @@ let fileSelector = {
 
 
   _container(width, height){
+    // Mein Container
     let container = $('<div/>').addClass('fileSelectorBox');
+    // Headbar
+    let head = $('<header/>');
+    head.append($('<span/>').text('Select a file ...'));
+    head.append($('<button/>').html('<i class="fa fa-times" aria-hidden="true"></i>').addClass('close'));
+    // Searchbar
+    let search = $('<div/>').addClass('search-container');
+    search.append($('<input/>').attr('placeholder', 'Search'));
+    // File-Container
+    let subcontainer = $('<div/>').addClass('container');
+
+    let dircontainer = $('<div/>').addClass('dir-container');
+    dircontainer.append(fileSelector._folder('back'));
+    let filecontainer = $('<div/>').addClass('file-container');
+    filecontainer.append(fileSelector._file('test.jpg'));
+    filecontainer.append(fileSelector._file('test.jpg'));
+    filecontainer.append(fileSelector._file('test.jpg'));
+    filecontainer.append(fileSelector._file('test.jpg'));
+
+    subcontainer.append(dircontainer);
+    subcontainer.append(filecontainer);
+
+
+    // Add elements
+    container.append(head);
+    container.append(search);
+    container.append(subcontainer);
     return container;
+  },
+
+  _folder(name){
+    let folder = $('<div/>').addClass('dir').html('<i class="fa fa-folder" aria-hidden="true"></i><h3>'+name+"</h3>");
+    return folder;
+  },
+
+  _file(name){
+    let folder = $('<div/>').addClass('file').html('<i class="fa fa-file-image-o" aria-hidden="true"></i><h3>'+name+"</h3>");
+    return folder;
   }
 
 
