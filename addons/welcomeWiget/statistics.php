d70976b90e0851deafe8ea546fb44f1733d0b740
Khadim Fall <khadimfall0602@gmail.com>
2016-12-11 00:33:41 +0100

    some moibiule enhancemenmt

---
+++ addons/welcomeWiget/statistics.php
@@ -0,0 +1,36 @@
+<?php
+use KFall\oxymora\addons\iAddon;
+
+class welcomeWidget implements iAddon{
+
+  // ========================================
+  //  EVENTS
+  // ========================================
+
+  // Start/Stop Events
+  public function onInstallation(){
+
+  }
+
+  public function onEnable(){
+
+  }
+  public function onDisable(){
+
+  }
+
+  // CMS
+  public function onOpen(){
+
+  }
+  public function onTabChange($tab){
+
+  }
+
+  // Page
+  public function onPageOpen($page){
+
+  }
+
+
+}
