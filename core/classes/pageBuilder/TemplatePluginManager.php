abb411d2449d458e76014959a28bde437955e664
Khadim Fall <khadimfall0602@gmail.com>
2016-12-10 11:12:56 +0100

    fixed some strict errors

--- core/classes/pageBuilder/TemplatePluginManager.php
+++ core/classes/pageBuilder/TemplatePluginManager.php
@@ -3,15 +3,15 @@
 class TemplatePluginManager{
 
   public static function listPlugins($template, $showHidden = true){
-    $templatePath = ROOT_DIR."..\\template\\".$template."\\_plugins";
+    $templatePath = ROOT_DIR."../template/".$template."/_plugins";
     $all = scandir($templatePath);
     $dirs = [];
     foreach($all as $item){
-      $path = $templatePath."\\".$item;
+      $path = $templatePath."/".$item;
       if(strlen(trim($item, ".")) > 0 && is_dir($path)){
         $assoc['name'] = $item;
-        $assoc['config'] = json_decode(file_get_contents($path."\\config.json"), true);
-        $assoc['thumb'] = (file_exists($path."\\thumb.jpg"));
+        $assoc['config'] = json_decode(file_get_contents($path."/config.json"), true);
+        $assoc['thumb'] = (file_exists($path."/thumb.jpg"));
         $assoc['thumbUrl'] = ($assoc['thumb']) ? "template/".$template."/_plugins/".$item."/thumb.jpg" : null ;
         if($showHidden || $assoc['config']['visible']){
           $dirs[] = $assoc;
@@ -22,15 +22,15 @@ class TemplatePluginManager{
   }
 
   public static function findPlugin($template, $name){
-    $templatePath = ROOT_DIR."..\\template\\".$template."\\_plugins";
+    $templatePath = ROOT_DIR."../template/".$template."/_plugins";
     $all = scandir($templatePath);
     foreach($all as $item){
-      $path = $templatePath."\\".$item;
+      $path = $templatePath."/".$item;
       if(strlen(trim($item, ".")) > 0 && is_dir($path)){
         if($item == $name){
           $assoc['name'] = $item;
-          $assoc['config'] = json_decode(file_get_contents($path."\\config.json"), true);
-          $assoc['thumb'] = (file_exists($path."\\thumb.jpg"));
+          $assoc['config'] = json_decode(file_get_contents($path."/config.json"), true);
+          $assoc['thumb'] = (file_exists($path."/thumb.jpg"));
           $assoc['thumbUrl'] = ($assoc['thumb']) ? "template/".$template."/_plugins/".$item."/thumb.jpg" : null ;
           return $assoc;
         }
@@ -40,8 +40,8 @@ class TemplatePluginManager{
   }
 
   public static function loadPlugin($template, $name){
-    $templatePath = ROOT_DIR."..\\template\\".$template;
-    $file = $templatePath."\\_plugins\\$name\\$name.php";
+    $templatePath = ROOT_DIR."../template/".$template;
+    $file = $templatePath."/_plugins/$name/$name.php";
     $class = "template\\".$template."\\$name";
     if(file_exists($file)){
       require_once $file;
