55f8e144dec6e4b76f133c34d90c7cc583f31544
Khadim Fall <khadimfall0602@gmail.com>
2016-12-10 01:11:13 +0100

    nicer autloader

--- core/autoload.php
+++ core/autoload.php
@@ -4,13 +4,14 @@ function __autoload($className) {
 
   // Directories to search
   $classDirs = [
-    "classes\\",
-    "interfaces\\"
+    "classes/",
+    "interfaces/"
   ];
 
   // Check if file exists and require
   foreach ($classDirs as $dir) {
     $className = str_replace("KFall\\oxymora\\","",$className);
+    $className = str_replace("\\","/",$className);
     $path =  ROOT_DIR.$dir.$className.'.php';
     if (file_exists($path)) {
       require_once $path;
