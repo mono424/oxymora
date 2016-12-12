35bb8a437a685f2028fbdb654ca1772e7c77dbe0
Khadim Fall <khadimfall0602@gmail.com>
2016-12-13 00:09:10 +0100

    fullscreen features

--- template/oxymora_landingpage/_plugins/FullscreenFeatures/FullscreenFeatures.php
+++ template/oxymora_landingpage/_plugins/FullscreenFeatures/FullscreenFeatures.php
@@ -4,41 +4,74 @@ use KFall\oxymora\pageBuilder\template\iTemplatePluginSettings;
 
 class FullscreenFeatures implements iTemplatePlugin, iTemplatePluginSettings{
 
-  private $img;
-  private $title;
+  private $slides;
 
   private $htmlText = '
-  <header style="background: url({img}) no-repeat top center fixed;">
-  <div class="ov"></div>
-  {titles}
+  <div class="fsContainer">
+  <div class="slideContainer">
+  {slides}
+  </div>
+  </div>
 
-  <!-- <div class="arrow-down">
-  <i class="fa fa-angle-double-down" aria-hidden="true"></i>
-  </div> -->
-  </header>
-  <script>
+
+
+  <script type="text/javascript">
   (function(){
-    let head = $("header");
-    let textelements = $("header h1");
-    let currentItem = 0;
+    let slideContainer = $(\'.slideContainer\');
+    let slides = slideContainer.find(\'.slide\');
+    let slidesCount = slides.length;
+    let minTop = 0;
+    createHeight();
 
-    if(textelements.length > 1){console.log(textelements)
+    $(window).on(\'scroll\', function(e){
+      // Slide Position
+      let screenHeight = $(window).height();
+      let containerHeight = slideContainer.outerHeight();
+      let slidesHeight = $(slides[0]).outerHeight();
+      let maxTop = containerHeight - slidesHeight;
+      let scrollPos = $(window).scrollTop();
+      let containerScroll = scrollPos - slideContainer.offset().top;
+      let slidePos = containerScroll + (screenHeight - slidesHeight) / 2;
+      slidePos = (slidePos < minTop) ? minTop  : slidePos;
+      slidePos = (slidePos > maxTop) ? maxTop  : slidePos;
+      slides.css(\'top\', slidePos+\'px\');
 
-      showNextLine();
+      // Slide Opacity
+      let percent = 100 / maxTop * slidePos;
+      slideVisible(percent);
+    });
 
-      function showNextLine(){
-        currentItem = (currentItem >= textelements.length) ? 0 : currentItem;
 
-        $(textelements).fadeOut(400);
-        $(textelements[currentItem]).fadeIn(800, function(){
-          setTimeout(function(){
-            currentItem++;
-            showNextLine();
-          }, $(textelements[currentItem]).data("time"));
-        });
+    function slideVisible(percent){
+      let info = [];
 
+      slides.each(function(){
+        let distance = Math.abs((percent - $(this).data(\'percent\')));
+        let opc = 1 / 100 * (100 - distance);
+        $(this).css(\'opacity\', opc);
+        info.push({distance, element:$(this)});
+      });
+
+      info.sort(function(a, b){
+        if(a.distance < b.distance) return -1;
+        if(a.distance > b.distance) return 1;
+        return 0;
+      });
+
+      for(let i=0;i<info.length;i++){
+        info[i].element.css(\'z-index\', info.length - i);
+        if(i == 0){info[i].element.find(\'div\').fadeIn(100);}else{
+          info[i].element.find(\'div\').fadeOut(100);
+        }
       }
+    }
 
+    function createHeight(nr){
+      let size = 0;
+      slides.each(function(){
+        size += $(this).outerHeight();
+      });
+      slideContainer.css(\'height\', size+"px");
     }
   })();
   </script>
@@ -50,15 +83,22 @@ class FullscreenFeatures implements iTemplatePlugin, iTemplatePluginSettings{
 
   public function getHtml(){
     $html = $this->htmlText;
-    $html = str_replace("{img}", $this->img, $html);
-    $html = str_replace("{titles}", $this->generateTitlesHtml(), $html);
+    $html = str_replace("{slides}", $this->generateTitlesHtml(), $html);
     return $html;
   }
 
   private function generateTitlesHtml(){
     $html = "";
-    foreach($this->titles as $title){
-      $html .= "<h1 data-time=\"".($title['time'] * 1000)."\">".$title['title']."</h1>\n";
+    $total = count($this->slides);
+    $i = 0;
+    foreach($this->slides as $slide){
+      $percent = 100 / ($total-1) * $i;
+      $html .= '<div class="slide" data-percent="'.$percent.'">
+      <img class="image-shadow" src="'.$slide['image'].'" alt="">
+      <img class="image" src="'.$slide['image'].'" alt="">
+      <div>'.$slide['content'].'</div>
+      </div>';
+      $i++;
     }
     return $html;
   }
