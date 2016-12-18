b4a277ef27e2f5ade61159a002fd591111e78e9c
Khadim Fall <khadimfall0602@gmail.com>
2016-12-18 01:04:59 +0100

    improivet features thang

--- template/oxymora_landingpage/_plugins/FullscreenFeatures/FullscreenFeatures.php
+++ template/oxymora_landingpage/_plugins/FullscreenFeatures/FullscreenFeatures.php
@@ -17,64 +17,43 @@ class FullscreenFeatures implements iTemplatePlugin, iTemplatePluginSettings{
 
 
   <script type="text/javascript">
-  (function(){
+
+
+  $(window).on("load",function(){
     let slideContainer = $(\'.slideContainer\');
+    let background = $(\'.slideContainer .background\');
     let slides = slideContainer.find(\'.slide\');
-    let slidesCount = slides.length;
-    let minTop = 0;
-    createHeight();
+
+    console.log(123);
+    $(slides[0]).css("display", "inline-block");
+    let slidesHeight = slides[0].offsetHeight;console.log(slides[0].offsetHeight)
+    let heightBuffer = slidesHeight * 0.2;
+    setHeight();
+
+
+    $(window).on(\'resize\', function(e){
+      slidesHeight = slides[0].offsetHeight;console.log(slides[0].offsetHeight)
+      heightBuffer = slidesHeight * 0.2;
+      setHeight();
+    });
 
     $(window).on(\'scroll\', function(e){
       // Slide Position
       let screenHeight = $(window).height();
-      let containerHeight = slideContainer.outerHeight();
-      let slidesHeight = $(slides[0]).outerHeight();
-      let maxTop = containerHeight - slidesHeight;
       let scrollPos = $(window).scrollTop();
       let containerScroll = scrollPos - slideContainer.offset().top;
-      let slidePos = containerScroll + (screenHeight - slidesHeight) / 2;
-      slidePos = (slidePos < minTop) ? minTop  : slidePos;
-      slidePos = (slidePos > maxTop) ? maxTop  : slidePos;
-      slides.css(\'top\', slidePos+\'px\');
-
-      // Slide Opacity
-      let percent = 100 / maxTop * slidePos;
-      slideVisible(percent);
+      let customScroll = containerScroll + screenHeight / 2 - slidesHeight / 2;
+      customScroll = (customScroll > heightBuffer) ? heightBuffer : customScroll;
+      customScroll = (customScroll < 0) ? 0 : customScroll;
+      slides.css("top",customScroll + "px");
     });
 
-
-    function slideVisible(percent){
-      let info = [];
-
-      slides.each(function(){
-        let distance = Math.abs((percent - $(this).data(\'percent\')));
-        // let opc = 1 / 100 * (100 - distance);
-        // $(this).css(\'opacity\', opc);
-        info.push({distance, element:$(this)});
-      });
-
-      info.sort(function(a, b){
-        if(a.distance < b.distance) return -1;
-        if(a.distance > b.distance) return 1;
-        return 0;
-      });
-
-      for(let i=0;i<info.length;i++){
-        info[i].element.css(\'z-index\', info.length - i);
-        if(i == 0){info[i].element.find(\'div\').fadeIn(100);}else{
-          info[i].element.find(\'div\').fadeOut(100);
-        }
-      }
+    function setHeight(){
+      slideContainer.css(\'height\', (slidesHeight+heightBuffer)+"px");
+      slideContainer.css(\'margin-top\', "-"+heightBuffer+"px");
+      background.css(\'top\', (heightBuffer)+"px");
     }
-
-    function createHeight(nr){
-      let size = 0;
-      slides.each(function(){
-        size += $(this).outerHeight();
-      });
-      slideContainer.css(\'height\', size+"px");
-    }
-  })();
+  });
   </script>
   ';
 
