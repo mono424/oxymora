ce756b92803899a4f909d20318d00299e51c8d54
Khadim Fall <khadimfall0602@gmail.com>
2017-01-09 04:01:43 +0100

    started with setup

---
+++ setup/index.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>Oxymora Setup</title>
+    <link rel="stylesheet" href="css/master.css">
+  </head>
+  <body>
+    <div class="wrapper">
+      <header>
+        <h1>Getting started</h1>
+      </header>
+
+      <section data-page="start" class="active">
+        <p>Welcome to Oxymora Setup, follow few easy steps to set me up!</p>
+        <p>First of all, do you have a Backup which you want to load, or do you want to setup Oxymora for the first time?</p>
+        <button type="button" name="setup">Setup Oxymora from the scratch!</button>
+        <button type="button" name="backup">I do got a Backup</button>
+      </section>
+
+      <section  data-page="setup">
+
+      </section>
+
+      <section  data-page="backup">
+
+      </section>
+    </div>
+  </body>
+</html>
