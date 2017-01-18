8b6e5d7b181301074552f414435967723c6f8558
Khadim Fall <khadimfall0602@gmail.com>
2017-01-18 18:55:11 +0100

    windows create backup fix

--- admin/php/ajax_backup.php
+++ admin/php/ajax_backup.php
@@ -11,14 +11,15 @@ if(isset($_GET['create'])){
   $password = (isset($_GET['password']) && !empty($_GET['password'])) ? $_GET['password'] : "";
   try{
     $file = Exporter::export($exportConfig, $password);
-    $file = explode('/', $file);
+    // $file = explode('/', $file);
     $answer['type'] = "success";
-    $answer['message'] = $file[count($file)-1];
+    // $answer['message'] = $file[count($file)-1];
+    $answer['message'] = basename($file);
   } catch (Exception $e) {
     $answer['type'] = "error";
     $answer['message'] = $e->getMessage();
   }
-}elseif(isset($_GET['download']) && isset($_GET['file']) && preg_match('/^[a-zA-Z0-9]*$/', $_GET['file']) && file_exists(TEMP_DIR."/exports/".$_GET['file'])){
+}elseif(isset($_GET['download']) && isset($_GET['file']) && preg_match('/^[a-zA-Z0-9]*(\.tmp)?$/', $_GET['file']) && file_exists(TEMP_DIR."/exports/".$_GET['file'])){
   header("Content-Disposition: attachment; filename=oxymora_backup.oxybackup");
   header("Content-Type: binary/octet-stream");
   readfile(TEMP_DIR."/exports/".$_GET['file']);
