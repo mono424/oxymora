2bbe47f4363c5833d147efa510f059a8d905806a
Khadim Fall <khadimfall0602@gmail.com>
2017-01-26 20:22:06 +0100

    errormessage update and implemented into file

--- file.php
+++ file.php
@@ -1,5 +1,6 @@
 <?php
 use KFall\oxymora\fileSystem\FileManager;
+use KFall\oxymora\pageBuilder\ErrorBuilder;
 require_once 'core/core.php';
 
 $file = isset($_GET['file']) ? $_GET['file'] : notFound();
@@ -20,22 +21,16 @@ try{
 
 
 function notFound(){
-  header('HTTP/1.0 404 Not Found');
-  echo "<h1>Error 404 Not Found</h1>";
-  echo "The page that you have requested could not be found.";
+  ErrorBuilder::printOut('404', 'Error 404 Not Found');
   die();
 }
 
 function internalError($err){
-  header('HTTP/1.0 500 Internal Server Error');
-  echo "<h1>Error 500 Internal Server Error</h1>";
-  echo $err;
+  ErrorBuilder::printOut('500', '500 Internal Server Error');
   die();
 }
 
 function accessDenied(){
-  header('HTTP/1.0 403 Forbidden');
-  echo "<h1>Error 403 Forbidden</h1>";
-  echo "You don't have permission to access the requested page.";
+  ErrorBuilder::printOut('403', 'Error 403 Forbidden');
   die();
 }
