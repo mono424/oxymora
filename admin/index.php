744b5c0846ca00855acf1fd98e73428a1fb79a6f
Khadim Fall <khadimfall0602@gmail.com>
2017-02-04 00:17:50 +0100

    quick fix

--- admin/index.php
+++ admin/index.php
@@ -17,16 +17,16 @@ AddonManager::triggerEvent(ADDON_EVENT_OPEN, null);
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
   <link rel="stylesheet" href="assets/dist/css/dashboard.min.css" media="screen">
   <script src="https://code.jquery.com/jquery-3.1.1.min.js" integrity="sha256-hVVnYaiADRTO2PzUGmuLJr8BLUSjGIZsDYGmIJLv2b8=" crossorigin="anonymous"></script>
+  <script type="text/javascript">
+  let ROOT_DIR = '<?php echo dirname($_SERVER['PHP_SELF']); ?>';
   <?php
   if(isset($_GET['p'])){
     ?>
-    <script type="text/javascript">
-    let ROOT_DIR = '<?php echo dirname($_SERVER['PHP_SELF']); ?>';
     let START_PAGE = '<?php echo htmlspecialchars($_GET['p']); ?>';
-    </script>
     <?php
   }
   ?>
+  </script>
 </head>
 <body>
 
